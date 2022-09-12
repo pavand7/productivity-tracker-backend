984f5a694ae4c2c2529f7728cd365831a0820251
pavand7 <pavan.cr.d7@gmail.com>
2022-09-12 15:51:35 +0530

    change user to userID in GetUsersIdealData queryset

--- userapi/viewsets.py
+++ userapi/viewsets.py
@@ -75,7 +75,7 @@ class GetUsersIdealData(viewsets.ModelViewSet):
 
     def get_queryset(self):
         userID = self.kwargs['userID']
-        return models.IdealData.objects.filter(user__exact = userID)
+        return models.IdealData.objects.filter(userID__exact = userID)
 
 class GetUsersPosts(viewsets.ModelViewSet):
     serializer_class = serializers.UserPostSerializer
