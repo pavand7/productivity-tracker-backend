e4a5b3f3ab3971c7e266f8c1a63ac1cd8cd96582
pavand7 <pavan.cr.d7@gmail.com>
2022-09-14 20:51:17 +0530

    Restrict get, getAll, post, update, delete on all viewSets

--- userapi/viewsets.py
+++ userapi/viewsets.py
@@ -1,7 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from . import models
 from . import serializers
-from datetime import datetime
 
 class BadgeViewSet(viewsets.ModelViewSet):
     queryset = models.Badge.objects.all()
@@ -11,43 +10,34 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
 
-class GetUserByEmail(viewsets.ModelViewSet):
+class GetUserByEmail(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.UserSerializer
 
     def get_queryset(self):
         email = self.kwargs['email']
         return models.User.objects.filter(email__exact = email)
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.Event.objects.all()
     serializer_class = serializers.EventSerializer
 
-# to get user's today's events - as local date is not there, use UTC.
-class GetUsersDaysEvents(viewsets.ModelViewSet):
-    serializer_class = serializers.EventSerializer
-
-    def get_queryset(self):
-        userID = self.kwargs['userID']
-        timestamp = int(self.kwargs['timestamp'])
-        return models.User.objects.get(userID = userID).events.all().filter(date__startswith =  str(datetime.fromtimestamp(timestamp).date()))
-
-class GetPostViewSet(viewsets.ModelViewSet):
+class GetPostViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.Post.objects.order_by('-timestamp')
     serializer_class = serializers.GetPostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = models.Post.objects.order_by('-timestamp')
     serializer_class = serializers.PostSerializer
 
-class PostLikesViewSet(viewsets.ModelViewSet):
+class PostLikesViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.PostLikes.objects.all()
     serializer_class = serializers.PostLikesSerializer
 
-class CommentViewSet(viewsets.ModelViewSet):
+class CommentViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.Comment.objects.all()
     serializer_class = serializers.CommentSerializer
 
-class CommentLikesViewSet(viewsets.ModelViewSet):
+class CommentLikesViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.CommentLikes.objects.all()
     serializer_class = serializers.CommentLikesSerializer
 
@@ -59,46 +49,46 @@ class IdealDataViewSet(viewsets.ModelViewSet):
     queryset = models.IdealData.objects.all()
     serializer_class = serializers.IdealDataSerializer
 
-class ProductivityViewSet(viewsets.ModelViewSet):
+class ProductivityViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = models.Productivity.objects.all()
     serializer_class = serializers.ProductivitySerializer
 
-class GetUsersFollowing(viewsets.ModelViewSet):
+class GetUsersFollowing(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.FollowingSerializer
 
     def get_queryset(self):
         userID = self.kwargs['userID']
         return models.Following.objects.filter(user_id__exact = userID)
 
-class GetUsersIdealData(viewsets.ModelViewSet):
+class GetUsersIdealData(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.IdealDataSerializer
 
     def get_queryset(self):
         userID = self.kwargs['userID']
         return models.IdealData.objects.filter(userID__exact = userID)
 
-class GetUsersPosts(viewsets.ModelViewSet):
+class GetUsersPosts(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.UserPostSerializer
 
     def get_queryset(self):
         userID = self.kwargs['userID']
         return models.Post.objects.filter(user_id__exact = userID)
 
-class GetPostsLikes(viewsets.ModelViewSet):
+class GetPostsLikes(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.PostLikesSerializer
 
     def get_queryset(self):
         postID = self.kwargs['postID']
         return models.PostLikes.objects.filter(post_id__exact = postID)
 
-class GetPostsComments(viewsets.ModelViewSet):
+class GetPostsComments(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.CommentSerializer
 
     def get_queryset(self):
         postID = self.kwargs['postID']
         return models.Comment.objects.filter(post_id__exact = postID)
 
-class GetCommentsLikes(viewsets.ModelViewSet):
+class GetCommentsLikes(mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.CommentLikesSerializer
 
     def get_queryset(self):
