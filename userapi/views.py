10d439bc266bc50f4e5cee0e933e71b45193696c
pavand7 <pavan.cr.d7@gmail.com>
2022-09-14 23:48:55 +0530

    store user's access and refresh tokens to avoid relogin

--- userapi/views.py
+++ userapi/views.py
@@ -13,9 +13,9 @@ import datetime
 from dateutil.tz import *
 import json
 
-# import os.path
-# from google.oauth2.credentials import Credentials
-# from google.auth.transport.requests import Request
+import os
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -27,27 +27,27 @@ def calendar(date = None, event = None):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    # creds = None
-    # # The file token.json stores the user's access and refresh tokens, and is
-    # # created automatically when the authorization flow completes for the first
-    # # time.
-    # if os.path.exists('token.json'):
-    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             './client_secret_new.json', SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open('token.json', 'w') as token:
-    #         token.write(creds.to_json())
-    
-    flow = InstalledAppFlow.from_client_secrets_file(
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
                 './client_secret_new.json', SCOPES)
-    creds = flow.run_local_server(port=0) # request login and calendar permissions from user
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    
+    # flow = InstalledAppFlow.from_client_secrets_file(
+    #             './client_secret_new.json', SCOPES)
+    # creds = flow.run_local_server(port=0) # request login and calendar permissions from user
 
     try:
         service = build('calendar', 'v3', credentials=creds)
